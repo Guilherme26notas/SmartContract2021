b2607ad6676d66e0e9f451d2d1bf03515d6da610
Guilherme26notas <92129706+Guilherme26notas@users.noreply.github.com>
2021-10-07 21:42:01 -0300

    cartao.sol

---
+++ cartao.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.8.9;
+    
+    contract CartaoDeVacinacao { 
+// autor: Guilherme
+
+string constant public nome = "Guilherme Silva";
+string public dose = "1 dose";
+string public unidade = "UBS MOOCA";
+uint public cnes = 789;
+uint public data = 10102021;
+uint private lote = 1234;
+string constant private fabricante = "coronavac";
+string public vacinador = "joana";
+bool public imunizado = false;
+address private wallet = 0x310FC232911DC18232184b18ed5aA2Cb5561cc70;
+
+        
+    }
